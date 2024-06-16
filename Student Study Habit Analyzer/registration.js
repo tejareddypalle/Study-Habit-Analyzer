a9c99f858d8f467c414737b4948aac2c48ff09eb
TEJA REDDY PALLE <168373029+tejareddypalle@users.noreply.github.com>
2024-06-16 18:19:15 -0500

    Delete Student Study Habit Analyzer directory

--- Student Study Habit Analyzer/registration.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    
-    // Simulate registration process
-    localStorage.setItem(username, password);
-    document.getElementById('registrationMessage').textContent = 'Registration successful';
-});
